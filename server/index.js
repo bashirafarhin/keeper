58b5f548e3037c26292c4b30f525006103fa8fc3
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2025-01-19 18:58:53 +0530

    rerouting all paths to index.html file

--- server/index.js
+++ server/index.js
@@ -6,6 +6,9 @@ import "./Database/connection.js";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
+import path from "path";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 import { connectToDB } from "./Database/connection.js";
 
 env.config();
@@ -30,6 +33,13 @@ app.use(express.json());
 // Serve API routes
 app.use("/user", userRouter);
 app.use("/", authRouter);
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+app.use(express.static(path.join(__dirname, 'client/dist')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/dist', 'index.html'));
+});
 
 app.get('/',(req,res)=>{
   res.send('Hello Developer!')
