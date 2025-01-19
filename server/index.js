cb795030fc2b8b1ebf784fce9763ba6eca9ce305
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2025-01-19 19:03:30 +0530

    check

--- server/index.js
+++ server/index.js
@@ -36,9 +36,10 @@ app.use("/", authRouter);
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-app.use(express.static(path.join(__dirname, 'client/dist')));
+app.use(express.static(path.join(__dirname, '../client/dist')));
+console.log(path.join(__dirname, '../client/dist', 'index.html'))
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/dist', 'index.html'));
+  res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
 });
 
 app.get('/',(req,res)=>{
